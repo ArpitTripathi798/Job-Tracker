e404fd42ee1ea549f1ba354984b28835fe28462a
Arpit Tripathi <arpit786tripathi@gmail.com>
2026-01-03 22:45:33 +0530

    Fix register route and update frontend routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import AddJob from "./pages/AddJob";
 
@@ -7,13 +9,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Auth */}
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* App */}
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-job" element={<AddJob />} /> {/* 🔥 THIS */}
+        <Route path="/add-job" element={<AddJob />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 
