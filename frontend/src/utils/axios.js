1750c788f273143d6826e3388dec5fae82ced744
Arpit Tripathi <arpit786tripathi@gmail.com>
2026-01-04 09:17:21 +0530

    Fix frontend to use correct Render backend URL

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://job-tracker-backend.onrender.com/api",
+  baseURL:"https://job-tracker-backend-kmux.onrender.com/api",
+
 
 });
 
